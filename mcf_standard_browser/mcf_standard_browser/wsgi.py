ca7c179da61c1a55f89d1b1ac92909c1be01d617
andy-d-palmer <palmer@embl.de>
2016-01-12 19:43:34 +0100

    django initialise

---
+++ mcf_standard_browser/mcf_standard_browser/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for mcf_standard_browser project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mcf_standard_browser.settings")
+
+application = get_wsgi_application()
