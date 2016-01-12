cc6ef70f89e4a91b12e09b2368b8bebe44812d16
andy-d-palmer <palmer@embl.de>
2016-01-12 22:00:09 +0100

    added Dataset model & FT - linked to Standard (ManyToMany  standard <-> dataset)

--- mcf_standard_browser/standards_review/models.py
+++ mcf_standard_browser/standards_review/models.py
@@ -3,9 +3,21 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class Dataset(models.Model):
+    name = models.TextField(default="")
+
+    def __str__(self):
+        return self.name
+
+
 class Standard(models.Model):
     name = models.TextField(default = "")
     sum_formula = models.TextField(null=True)
     MCFID = models.TextField(default="")
     # todo(An)
     # Inchi, ChEBI
+    datasets_present_in = models.ManyToManyField(Dataset)
+
+    def __str__(self):
+        return self.name
+
--- mcf_standard_browser/standards_review/tests.py
+++ mcf_standard_browser/standards_review/tests.py
@@ -1,8 +1,36 @@
 from django.test import TestCase
-from models import Standard
+from models import Standard, Dataset
 import unittest
 # Create your tests here.
-class StandardModel(TestCase):
+
+class DatasetModelTest(TestCase):
+    def test_add_dataset(self):
+        Dataset(name='TestDataset').save()
+        self.assertEqual(Dataset.objects.all().count(),1)
+
+
+class StandardModelTest(TestCase):
     def test_add_standard(self):
-        Standard(name='TestStandard',sum_formula="C1H2O3", MCFID = "000").save()
+        Standard(name='TestStandard',sum_formula="C1H2O3", MCFID = "0").save()
         self.assertEqual(Standard.objects.all().count(),1)
+
+    def test_standard_and_dataset(self):
+        # create some datasets
+        d1 = Dataset(name='Dataset1')
+        d1.save()
+        d2 = Dataset(name='Dataset1')
+        d2.save()
+        # create some standards
+        s1 = Standard(name='Standard1',sum_formula="C1H2O3", MCFID = "00")
+        s1.save()
+        s1.datasets_present_in.add(d1)
+        s1.datasets_present_in.add(d2)
+        s2 = Standard(name='Standard2',sum_formula="C1H2O3", MCFID = "000")
+        s2.save()
+        s2.datasets_present_in.add(d2)
+        s2.datasets_present_in.create(name='Dataset3')
+        s1.save()
+        s2.save()
+        self.assertEqual(Standard.objects.all().count(),2)
+        self.assertEqual(Dataset.objects.all().count(),3)
+        self.assertEqual(d2.standard_set.all().count(),2)
