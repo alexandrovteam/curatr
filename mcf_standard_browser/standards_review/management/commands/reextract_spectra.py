aba850cac5d5c461c9391d3d0f18403da4c71868
andy-d-palmer <palmer@embl.de>
2017-04-05 16:16:53 +0200

    adding option to swap path

--- mcf_standard_browser/standards_review/management/commands/reextract_spectra.py
+++ mcf_standard_browser/standards_review/management/commands/reextract_spectra.py
@@ -15,16 +15,32 @@ class Command(BaseCommand):
             default=False,
             help='Just check which files are present',
         )
+
+        parser.add_argument(
+            '--path_swap',
+            action='store_true',
+            dest='path_swap_new',
+            default=False,
+            help='Remove existing path and replace with this directory',
+        )
+
+
     def handle(self, *args, **options):
         datasets = Dataset.objects.all()
         for dataset in datasets:
             try:
                 if options['check_files']:
-                    logging.debug((dataset.path, os.path.exists(dataset.path)))
+                    path = dataset.path
+                    if options['path_swap_new']:
+                        path = self.path_swap(dataset.path, options["path_swap_old"], options["path_swap_new"])
+                    logging.debug((path, os.path.exists(path)))
                 else:
                     logging.debug(dataset.path)
                     update_spectrum_from_file(dataset)
             except Exception as e:
                 logging.error("Error updating spectrum")
                 logging.error(e, exc_info=True)
-        return 0
+
+    def path_swap(self, path, path_swap_new):
+        path = os.path.split(path)[1]
+        return os.path.join(path_swap_new, path)
