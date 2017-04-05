006570afeb69d6ec478bab83ce7418fd9f21f425
andy-d-palmer <palmer@embl.de>
2017-04-05 15:30:35 +0200

    added quick check option before reextracting spectra

--- mcf_standard_browser/standards_review/management/commands/reextract_spectra.py
+++ mcf_standard_browser/standards_review/management/commands/reextract_spectra.py
@@ -1,17 +1,30 @@
 from django.core.management.base import BaseCommand, CommandError
 import logging
-import traceback
+import os
 from standards_review.models import Dataset
 from standards_review.tools import update_spectrum_from_file
 
 class Command(BaseCommand):
     help = 'Reinsert spectra into the database for all data files'
     def handle(self, *args, **options):
+        def add_arguments(self, parser):
+            # Named (optional) arguments
+            parser.add_argument(
+                '--check_files',
+                action='store_true',
+                dest='check_files',
+                default=False,
+                help='Just check which files are present',
+            )
+
         datasets = Dataset.objects.all()
         for dataset in datasets:
             try:
                 logging.debug("Updating dataset {}".format(dataset))
-                update_spectrum_from_file(dataset)
+                if options['check_files']:
+                    logging.debug(dataset.path, os.path.exists(dataset.path))
+                else:
+                    update_spectrum_from_file(dataset)
             except Exception as e:
                 logging.error("Error updating spectrum")
                 logging.error(e, exc_info=True)
