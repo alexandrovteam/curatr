448743a57574d392e6c2963df78885e5a8a00687
andy-d-palmer <palmer@embl.de>
2017-06-16 15:40:11 +0200

    export process into two: write files, serve files. write files is called daily with celery

--- mcf_standard_browser/mcf_standard_browser/settings_template.py
+++ mcf_standard_browser/mcf_standard_browser/settings_template.py
@@ -163,8 +163,7 @@ INSTITUTE_NAME= 'MY INSTITUTE'
 MASSBANK_INSTITUTE_CODE = 'DMY' # two or three letter code for massbank export
 EXPORT_LICENCE = "CC BY"
 VALID_EXPORT_FORMATS = ['massbank', 'metabolights', 'tsv', 'mgf', 'msp']
-SPECTRA_LIFETIME = False # number of months to keep old spectra in the database, False = Forever
-
+SITE_URL = "http://my_domain.edu"
 
 SETTINGS_EXPORT = [
     'INSTITUTE_NAME',
@@ -172,5 +171,6 @@ SETTINGS_EXPORT = [
     'MEDIA_URL',
     'EXPORT_LICENCE',
     'VALID_EXPORT_FORMATS',
-    'SPECTRA_LIFETIME'
+    'SPECTRA_LIFETIME',
+    'SITE_URL'
 ]
