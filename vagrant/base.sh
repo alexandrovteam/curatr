55ed256fe08710e866898809b7ae438e4eadbef0
andy-d-palmer <palmer@embl.de>
2017-09-20 14:58:41 +0200

    updated vagrant provisioning

--- vagrant/base.sh
+++ vagrant/base.sh
@@ -5,10 +5,14 @@ echo "deb http://www.rabbitmq.com/debian/ testing main" >> /etc/apt/sources.list
 curl http://www.rabbitmq.com/rabbitmq-signing-key-public.asc | sudo apt-key add -
 apt-get update
 apt-get -y install git
-echo "export PATH=$HOMEPATH/miniconda/bin:\$PATH" >> $HOMEPATH/.bashrc
-export PATH=$HOMEPATH/miniconda/bin:$PATH
+apt-get install -y --force-yes rabbitmq-server
+service rabbitmq-server start
 
 wget https://repo.continuum.io/miniconda/Miniconda2-latest-Linux-x86_64.sh
 bash Miniconda2-latest-Linux-x86_64.sh -b -p $HOMEPATH/miniconda
+
+ln -s $HOMEPATH/miniconda/bin/conda /usr/local/bin/conda
+ln -s $HOMEPATH/miniconda/bin/activate /usr/local/bin/activate
+ln -s $HOMEPATH/miniconda/bin/deactivate /usr/local/bin/deactivate
 
 chown -R vagrant:vagrant $HOMEPATH
--- vagrant/setup.sh
+++ vagrant/setup.sh
@@ -9,14 +9,11 @@ pip install pip-tools
 conda install -y -c clyde_fare openbabel=2.3.2
 pip-sync
 
-apt-get install -y --force-yes rabbitmq-server
-
 cp /home/vagrant/curatr/mcf_standard_browser/mcf_standard_browser/settings_template.py /home/vagrant/curatr/mcf_standard_browser/mcf_standard_browser/settings.py
-cd curatr/mcf_standard_browser
+cd ~/curatr/mcf_standard_browser
 python manage.py migrate
 echo "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@example.com', 'pass')" | python manage.py shell
 python manage.py migrate djcelery
-service rabbitmq-server start
 
 screen -dmS celery -L bash -c "source activate venv; python manage.py celeryd"
 screen -dmS curatr -L bash -c "source activate venv; python manage.py runserver [::]:8000"
