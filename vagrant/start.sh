40e851eeefa81b401ee9d94b2c5bc5dc548a192f
andy-d-palmer <palmer@embl.de>
2017-09-20 11:43:57 +0200

    split provisioning into root/user files

--- vagrant/start.sh
+++ vagrant/start.sh
@@ -10,29 +10,5 @@ export PATH=$HOMEPATH/miniconda/bin:$PATH
 
 wget https://repo.continuum.io/miniconda/Miniconda2-latest-Linux-x86_64.sh
 bash Miniconda2-latest-Linux-x86_64.sh -b -p $HOMEPATH/miniconda
-source $HOMEPATH/.bashrc
-
-git clone https://github.com/alexandrovteam/curatr
-cd curatr
-conda create -y -n venv python=2.7
-source activate venv
-pip install --upgrade pip
-pip install pip-tools
-conda install -y -c clyde_fare openbabel=2.3.2
-pip-sync
-
-apt-get install -y --force-yes rabbitmq-server
-
-cp /home/vagrant/curatr/mcf_standard_browser/mcf_standard_browser/settings_template.py /home/vagrant/curatr/mcf_standard_browser/mcf_standard_browser/settings.py
 
 chown -R vagrant:vagrant $HOMEPATH
-sudo -i -u vagrant
-
-cd /$HOMEPATH/curatr/mcf_standard_browser
-python manage.py migrate
-echo "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@example.com', 'pass')" | python manage.py shell
-python manage.py migrate djcelery
-service rabbitmq-server start
-
-screen -dmS celery -L bash -c "source activate venv; python manage.py celeryd"
-screen -dmS curatr -L bash -c "source activate venv; python manage.py runserver [::]:8000"
