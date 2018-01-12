d61b813cb06d935607bab58bc01a60b46a6bbae2
andy-d-palmer <palmer@embl.de>
2018-01-12 14:13:54 +0100

    registered molecules with the admin page

--- mcf_standard_browser/standards_review/admin.py
+++ mcf_standard_browser/standards_review/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from .models import Standard,Dataset, FragmentationSpectrum, Adduct, Xic, Molecule, LcInfo, MsInfo
 # Register your models here.
 admin.site.register(Standard)
+admin.site.register(Molecule)
 admin.site.register(Adduct)
 admin.site.register(Dataset)
 admin.site.register(FragmentationSpectrum)
